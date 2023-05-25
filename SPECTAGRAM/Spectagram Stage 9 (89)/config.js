42ed5e8e02c37dbd85e1cc7419ad8113d14ff400
codervedant <74607608+codervedant@users.noreply.github.com>
2023-05-25 20:27:18 +0530

    Add files via upload

---
+++ SPECTAGRAM/Spectagram Stage 9 (89)/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBHzilpgXOaP2P2f1zJ_GFOButkpTDwRR4",
+  authDomain: "storytelling-dc89d.firebaseapp.com",
+  databaseURL: "https://spectagram-93dc6-default-rtdb.firebaseio.com/",
+  projectId: "storytelling-dc89d",
+  storageBucket: "storytelling-dc89d.appspot.com",
+  messagingSenderId: "322708584017",
+  appId: "1:322708584017:web:6cf03830fe8e2838cdea70"
+};
