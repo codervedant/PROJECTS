d0f5caf9931ec38aadafa2686a619f5e4f48c822
codervedant <74607608+codervedant@users.noreply.github.com>
2023-05-25 20:25:05 +0530

    Add files via upload

---
+++ SPECTAGRAM/Spectagram Stage 6 (86)/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBz5t_FcUqudWKN3fuM53oKKBo5yheBPrA",
+  authDomain: "spectagram-93dc6.firebaseapp.com",
+  projectId: "spectagram-93dc6",
+  databaseURL: 'https://spectagram-93dc6-default-rtdb.firebaseio.com/',
+  storageBucket: "spectagram-93dc6.appspot.com",
+  messagingSenderId: "768723665795",
+  appId: "1:768723665795:web:d5f741027ca779e4c8ecb6"
+};
