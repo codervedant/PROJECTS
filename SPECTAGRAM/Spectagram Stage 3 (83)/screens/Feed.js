6fc15636619703f1c21cc7ce8d89606a383c615a
codervedant <74607608+codervedant@users.noreply.github.com>
2023-05-25 20:22:40 +0530

    Add files via upload

---
+++ SPECTAGRAM/Spectagram Stage 3 (83)/screens/Feed.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  ScrollView,
+} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import PostCard from './PostCard';
+
+import { FlatList } from 'react-native-gesture-handler';
+
+let posts = require('./temp_posts.json');
+
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  renderItem = ({ item: post }) => {
+    return <PostCard post={post} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <SafeAreaView style={styles.droidSafeArea} />
+        <View style={styles.appTitle}>
+          <View style={styles.appIcon}>
+            <Image
+              source={require('../assets/logo.png')}
+              style={styles.iconImage}></Image>
+          </View>
+          <View style={styles.appTitleTextContainer}>
+            <Text style={styles.appTitleText}>Spectagram</Text>
+          </View>
+        </View>
+        <View style={styles.cardContainer}>
+          <FlatList
+            keyExtractor={this.keyExtractor}
+            data={posts}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.06,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    margin: 10,
+    alignItems: 'center',
+  },
+  appIcon: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconImage: {
+    width: 51,
+    height: 51,
+  },
+  appTitleTextContainer: {
+    flex: RFValue(0.6),
+    justifyContent: 'center',
+  },
+  appTitleText: {
+    color: 'white',
+    fontSize: RFValue(45),
+    fontWeight: 'bold',
+    justifyContent: 'center',
+  },
+  cardContainer: {
+    flex: 0.85,
+  },
+});
---
+++ SPECTAGRAM/Spectagram Stage 5 (85)/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBz5t_FcUqudWKN3fuM53oKKBo5yheBPrA",
+  authDomain: "spectagram-93dc6.firebaseapp.com",
+  projectId: "spectagram-93dc6",
+  storageBucket: "spectagram-93dc6.appspot.com",
+  messagingSenderId: "768723665795",
+  appId: "1:768723665795:web:d5f741027ca779e4c8ecb6"
+};
